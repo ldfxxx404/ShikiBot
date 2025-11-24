1113110ba8eec69cd58580435933924c1c22a397
ldfxxx404 <limur9990@gmail.com>
2025-11-24 11:31:22 +0300

    feat: add ability for searching anime

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,3 +1,3 @@
 GET_ANIME_INFO_URL = "https://shikimori.one/api/animes/30"
 GET_RANDOM_ANIME_URL = "https://shikimori.one/api/animes/?order=random"
-# SEARCH_ANIME_URL = 'https://shikimori.one/api/animes/?search={name}'
+SEARCH_ANIME_URL = 'https://shikimori.one/api/animes'
--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,21 @@
 import requests
-from endpoints import GET_RANDOM_ANIME_URL
+from endpoints import GET_RANDOM_ANIME_URL, GET_ANIME_INFO_URL, SEARCH_ANIME_URL
 from headers import HEADERS
-
+# /?search={name}'
 
 def fetch_random_anime_data():
     try:
         response = requests.get(GET_RANDOM_ANIME_URL, headers=HEADERS)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except (requests.RequestException, ValueError):
+        return None
+
+
+def fetch_anime_info(name: str):
+    try:
+        response = requests.get(f"{SEARCH_ANIME_URL}/?search={name}", headers=HEADERS)
         response.raise_for_status()
         data = response.json()
         return data
@@ -27,30 +37,17 @@ def get_random_anime():
         f"Ссылка: https://shikimori.one{random_anime_url}\n\n"
     )
 
-# def get_anime_preview():
-#     anime_info = requests.get(GET_ANIME_INFO_URL, headers=HEADERS)
-#     prewiev_data = anime_info.json()
-#     image_path = prewiev_data["image"]["original"]
-#     return f"https://shikimori.one{image_path}"
 
-
-# def get_anime_name():
-#     anime_info = requests.get(GET_ANIME_INFO_URL, headers=HEADERS)
-#     anime_data = anime_info.json()
-#     anime_data_name = anime_data.get("russian")
-#     return f"Название аниме: {anime_data_name}"
-
-
-# def get_anime_score():
-#     anime_info = requests.get(GET_ANIME_INFO_URL, headers=HEADERS)
-#     anime_data = anime_info.json()
-#     anime_data_score = anime_data.get("score")
-#     return f"Оценка: {anime_data_score}"
-
-
-# def get_anime_episodes():
-#     anime_info = requests.get(GET_ANIME_INFO_URL, headers=HEADERS)
-#     anime_data = anime_info.json()
-#     anime_data_episodes = anime_data.get("episodes")
-#     return f"Эпизоды: {anime_data_episodes}"
-
+def get_anime_info(name):
+    data = fetch_anime_info(name)
+    anime = data[0]
+    anime_name = anime["russian"]
+    anime_url = anime["url"]
+    anime_score = anime["score"]
+    anime_episodes = anime["episodes"]
+    return (
+        f"Название аниме: {anime_name}\n\n"
+        f"Эпизоды: {anime_episodes}\n\n"
+        f"Оценка аниме: {anime_score}\n\n"
+        f"Ссылка: https://shikimori.one{anime_url}\n\n"
+    )
