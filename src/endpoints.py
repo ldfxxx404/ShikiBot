fc2bc072c21f449785a2c31b13e5f6048cc2f625
ldfxxx404 <limur9990@gmail.com>
2025-11-24 13:37:59 +0300

    chore:  minor changes

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,3 +1,2 @@
-GET_ANIME_INFO_URL = "https://shikimori.one/api/animes/30"
+SEARCH_ANIME_URL = "https://shikimori.one/api/animes"
 GET_RANDOM_ANIME_URL = "https://shikimori.one/api/animes/?order=random"
-SEARCH_ANIME_URL = 'https://shikimori.one/api/animes'
