e2e54044810bf8921a22d7ddb509aac86f4f3de3
ldfxxx404 <limur9990@gmail.com>
2025-11-26 19:35:42 +0300

    feat: add /similar command

--- src/commands.py
+++ src/commands.py
@@ -3,4 +3,5 @@ from aiogram.types import BotCommand
 BOT_COMMANDS: list[BotCommand] = [
     BotCommand(command="start", description="Start interact with bot"),
     BotCommand(command="random", description="Get random Shikimori anime"),
+    BotCommand(command="similar", description="Get similar anime by id")
 ]
