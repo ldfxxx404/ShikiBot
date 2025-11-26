6d3a6d2a62223868bfa8fb6a9eb521d241638083
ldfxxx404 <limur9990@gmail.com>
2025-11-27 00:08:37 +0300

    feat: add get_similar_anime method to fetch similar anime

--- src/api.py
+++ src/api.py
@@ -29,5 +29,16 @@ class ShikimoriApiClient:
         except Exception:
             return None
 
+    async def get_similar_anime(self, anime_id: int):
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    self._get_endpoint(f"/api/animes{anime_id}/similar"),
+                    params={"similar": anime_id},
+                ) as res:
+                    return await res.json()
+        except Exception:
+            return None
+
     def _get_endpoint(self, path: str):
         return f"{self.api_url}{path}"
